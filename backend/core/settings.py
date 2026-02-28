aa1af0eca528a623c464d7cddc142e459ec5d1a1
Willian Melo <willian.vieirademelo@gmail.com>
2026-02-28 16:22:52 -0300

    feat(tasks): add pagination to tasks endpoint

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -118,6 +118,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+
+    "DEFAULT_PAGINATION_CLASS": "common.pagination.DefaultPagination",
+    "PAGE_SIZE": 10,
 }
 
 # Joser
