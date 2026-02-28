4e38f1cca6fc4d53c9c1c8e2a7c466eea610bc31
Willian Melo <willian.vieirademelo@gmail.com>
2026-02-28 15:42:22 -0300

    feat(auth): require authentication for tasks and categories endpoints

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -110,12 +110,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Restframework
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": (        
-        "rest_framework.permissions.AllowAny",
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
     ),
 }
 
